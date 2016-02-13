af229d0dbde307c00249da80aaa37319c93f0596
Craig <craigmiller160>
2016-02-13 18:06:12 -0500

    C5RV-2 - Improved algorithms and added new methods to MultiValueMap. Also created special sub-class, SortedMultiValueMap, to maintain a sort order for both the keys and the values in each collection.

--- app/src/main/java/io/craigmiller160/contacts5/model/collection/MultiValueMap.java
+++ app/src/main/java/io/craigmiller160/contacts5/model/collection/MultiValueMap.java
@@ -11,26 +11,26 @@ import java.util.Set;
  */
 public class MultiValueMap<K,V> implements Map<K,Collection<V>> {
 
-    private Map<K, Collection<V>> map;
+    private Map<K,Collection<V>> map;
 
-    public MultiValueMap() {
+    public MultiValueMap(){
         map = new HashMap<>();
     }
 
     /**
      * Factory method that builds a new collection
      * to store the values of this map.
-     * <p/>
+     *
      * Subclasses can and should override this class
      * to use different collection types to achieve
      * different behaviors and results.
-     * <p/>
+     *
      * By default, it returns an ArrayList.
      *
      * @return the collection being used to store
      * values in this Map.
      */
-    protected Collection<V> getNewCollection() {
+    protected Collection<V> getNewCollection(){
         return new ArrayList<>();
     }
 
@@ -50,15 +50,33 @@ public class MultiValueMap<K,V> implements Map<K,Collection<V>> {
      * in all the collections in the Map.
      *
      * @return the total number of values in all collections
-     * in the Map.
+     *          in the Map.
      */
-    public int fullSize() {
+    public int fullSize(){
         Set<K> keySet = map.keySet();
         int total = 0;
-        for (K k : keySet) {
+        for(K k : keySet){
             total += map.get(k).size();
         }
         return total;
+    }
+
+    /**
+     * Get the size of the collection
+     * for the specified key.
+     *
+     * @param key the key of the collection
+     *            to get the size of.
+     * @return the size of the collection
+     * matching the key, or -1 if there is
+     * no collection matching the key.
+     */
+    public int valueSize(K key){
+        Collection<V> c = map.get(key);
+        if(c != null){
+            return c.size();
+        }
+        return -1;
     }
 
     @Override
@@ -81,9 +99,9 @@ public class MultiValueMap<K,V> implements Map<K,Collection<V>> {
     @Override
     public boolean containsValue(Object value) {
         Set<K> keySet = map.keySet();
-        for (K k : keySet) {
+        for(K k : keySet){
             Collection<V> c = map.get(k);
-            if (c.contains(value)) {
+            if(c.contains(value)){
                 return true;
             }
         }
@@ -97,7 +115,7 @@ public class MultiValueMap<K,V> implements Map<K,Collection<V>> {
      * @param c the colleciton to test if the Map contains.
      * @return true if the map contains a matching collection.
      */
-    public boolean containsCollection(Collection<V> c) {
+    public boolean containsCollection(Collection<V> c){
         return map.containsValue(c);
     }
 
@@ -109,21 +127,39 @@ public class MultiValueMap<K,V> implements Map<K,Collection<V>> {
     @Override
     public Collection<V> put(K key, Collection<V> c) {
         Collection<V> values = map.get(key);
-        if (values != null) {
+        if(values != null){
             values.addAll(c);
-        } else {
+        }
+        else{
             map.put(key, c);
         }
         return c;
     }
 
-    public V putValue(K key, V value) {
+    public V putValue(K key, V value){
         Collection<V> values = map.get(key);
-        if (values != null) {
-            values.add(value);
-            return value;
+        if(values == null){
+            values = getNewCollection();
+            map.put(key, values);
         }
-        return null;
+        values.add(value);
+        return value;
+    }
+
+    public V putValueInMultipleCollections(Collection<K> keys, V value){
+        for(K key : keys){
+            putValue(key, value);
+        }
+        return value;
+    }
+
+    public Collection<V> putMultipleValuesIntoCollection(K key, Collection<V> values){
+        Collection<V> collection = map.get(key);
+        if(collection == null){
+            collection = getNewCollection();
+        }
+        collection.addAll(values);
+        return put(key, collection);
     }
 
     @Override
@@ -132,31 +168,36 @@ public class MultiValueMap<K,V> implements Map<K,Collection<V>> {
     }
 
     /**
-     * Remove a single value from the collection
-     * matching the provided key.
+     * Remove a value form any collections that contain
+     * it in this map.
      *
-     * @param key   the key of the collection to remove
-     *              a value from.
      * @param value the value to remove from the collection.
-     * @return true if the value was successfully removed.
+     * @return the value that has been removed.
      */
-    public boolean removeValue(K key, V value) {
-        Collection<V> c = map.get(key);
-        if (c != null) {
-            return c.remove(value);
+    public V removeValue(V value){
+        Set<K> keySet = keySet();
+        for(K k : keySet){
+            Collection<V> c = map.get(k);
+            if(c != null){
+                c.remove(value);
+                if(c.size() == 0){
+                    remove(k);
+                }
+            }
         }
-        return false;
+        return value;
     }
 
     @Override
     public void putAll(Map<? extends K, ? extends Collection<V>> m) {
         Set<? extends K> keySet = m.keySet();
         Collection<V> template = getNewCollection();
-        for (K k : keySet) {
+        for(K k : keySet){
             Collection c = m.get(k);
-            if (c.getClass().equals(template.getClass())) {
+            if(c.getClass().equals(template.getClass())){
                 put(k, c);
-            } else {
+            }
+            else{
                 throw new IllegalArgumentException("Map must maintain the same type of Collection as its value. " +
                         "Expected: " + template.getClass().getName() + " Found: " + c.getClass().getName());
             }
@@ -182,5 +223,4 @@ public class MultiValueMap<K,V> implements Map<K,Collection<V>> {
     public Set<Entry<K, Collection<V>>> entrySet() {
         return map.entrySet();
     }
-
 }
