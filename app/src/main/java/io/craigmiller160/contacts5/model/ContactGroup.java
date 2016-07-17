8bf1c91edbdbd497276d23efc8bc9691c97dd88e
Craig Miller <craigmiller160@gmail.com>
2016-07-17 15:50:17 -0400

    Added a copyright notice to all source code files.

--- app/src/main/java/io/craigmiller160/contacts5/model/ContactGroup.java
+++ app/src/main/java/io/craigmiller160/contacts5/model/ContactGroup.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2016 ShadowAngler <craigmiller160@gmail.com> - All Rights Reserved
+ * Proprietary / Confidential
+ * Unauthorized copying, use, or redistribution of this file is prohibited
+ */
+
 package io.craigmiller160.contacts5.model;
 
 import android.net.Uri;
